fec3c8d24ef1e45d6b8a9306adae3d495edab729
Miguel Molina <mamolina26@espe.edu.ec>
2026-07-25 18:22:51 -0500

    fix: Cambiar puerto SMTP a 587 para evitar bloqueos en Render

--- backend/config/mail.config.js
+++ backend/config/mail.config.js
@@ -3,8 +3,8 @@ const { env } = require("./env");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // port 587 usa TLS (STARTTLS), por lo que secure debe ser false
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_APP_PASSWORD?.replace(/\s/g, "")
