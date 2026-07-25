89816d92b83be77df6f135b3016b0a46b524ecf9
Miguel Molina <mamolina26@espe.edu.ec>
2026-07-25 18:49:40 -0500

    fix: Usar 'service: gmail' nativo en nodemailer para evitar timeout

--- backend/config/mail.config.js
+++ backend/config/mail.config.js
@@ -2,9 +2,7 @@ const nodemailer = require("nodemailer");
 const { env } = require("./env");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // port 587 usa TLS (STARTTLS), por lo que secure debe ser false
+  service: "gmail",
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_APP_PASSWORD?.replace(/\s/g, "")
